f6792e32d0a111f3bfa52cb80766037de4bcc24e
DoThiThuyLinh <dothithuylinh123123@gmail.com>
2025-09-11 01:01:19 +0700

    Update: HW1

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Do Thi Thuy Linh\t12424017\t12424TN\tDoThiThuyLinh\tdothithuylinh123123@gmail.com");
+        Console.WriteLine("Đỗ Thị Thùy Linh\t12424017\t12424TN\tDoThiThuyLinh\tdothithuylinh123123@gmail.com");
     }
 }
