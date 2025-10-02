7d0945559c28025b4e2e52fd567c3ec632efff97
Đỗ Thị Thùy Linh <dothithuylinh123123@gmail.com>
2025-10-02 21:51:05 +0700

    Add files via upload

---
+++ HW4/HoaDonKhach.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quản_lý_khách_sạn
+{
+    class HoaDonKhach
+    {
+        private string tenkhach; // Tên khách
+        private int songay; // Số ngày ở
+        private Phong loaiPhong; // Loại phòng khách chọn
+
+        public void Nhap() // Nhập thông tin hóa đơn khách hàng
+        {
+            Console.WriteLine("Nhập thông tin hóa đơn khách hàng");
+            Console.Write("Họ tên: ");
+            tenkhach = Console.ReadLine(); // Nhập họ tên khách
+
+            Console.Write("Số ngày ở: ");
+            songay = int.Parse(Console.ReadLine()); // Nhập số ngày khách ở
+
+            Console.WriteLine("Chọn loại phòng A, B, C?");
+            char ch = char.Parse(Console.ReadLine()); // Nhập loại phòng
+
+            switch (char.ToUpper(ch))
+            {
+                case 'A': loaiPhong = new PhongA(songay); break; // Chọn phòng loại A
+                case 'B': loaiPhong = new PhongB(songay); break; // Chọn phòng loại B
+                case 'C': loaiPhong = new PhongC(songay); break; // Chọn phòng loại C
+            }
+        }
+
+        public void Hien() // Hiển thị thông tin hóa đơn khách
+        {
+            Console.WriteLine("Thông tin hóa đơn khách hàng");
+            Console.WriteLine("Họ tên khách: " + tenkhach);
+            Console.WriteLine("Số ngày ở: " + songay);
+            loaiPhong.Hien(); // Hiển thị thông tin phòng khách đã chọn
+        }
+    }
+
+}
--- HW4/Program.cs
+++ HW4/Program.cs
@@ -1,10 +1,25 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quản_lý_khách_sạn
 {
-    
-    private static void Main(string[] args)
+    internal class Program
     {
-        MyDate md1 = new MyDate(DateTime.Now.Day,DateTime.Now.Month, DateTime.Now.Year);
-        Console.WriteLine("{0}/{1}/{2}",md1.Day,md1.Month,md1.Year);
-        Console.ReadKey();
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            HoaDonKhach t = new HoaDonKhach(); // Tạo đối tượng HoaDonKhach
+            t.Nhap(); // Nhập thông tin khách
+            Console.Clear(); // Làm sạch màn hình
+            t.Hien(); // Hiển thị hóa đơn
+            Console.ReadLine(); // Dừng màn hình để xem kết quả
+
+            Console.ReadKey();
+        }
     }
 }
